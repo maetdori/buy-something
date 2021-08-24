f1ff5af070911462f814bdf662dc43c3bb64a6e1
LEE <odong2@ajou.ac.kr>
2021-08-24 19:03:13 +0900

    init

---
+++ src/main/java/com/maetdori/buysomething/BuySomethingApplication.java
@@ -0,0 +1,13 @@
+package com.maetdori.buysomething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySomethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySomethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maetdori/buysomething/BuySomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maetdori.buysomething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuySomethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
