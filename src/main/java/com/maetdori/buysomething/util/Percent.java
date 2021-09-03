e378f5b86c8a18eefcf331d04df486c1a612dd0a
LEE <odong2@ajou.ac.kr>
2021-09-03 17:49:26 +0900

    feat: 환불 기능 구현

--- src/main/java/com/maetdori/buysomething/util/Percent.java
+++ src/main/java/com/maetdori/buysomething/util/Percent.java
@@ -1,7 +1,7 @@
 package com.maetdori.buysomething.util;
 
 public class Percent {
-    public static int discountPercent(int amount, int percent) {
-        return amount / 100 * (100-percent);
+    public static int discountAmount(int amount, int percent) {
+        return amount / 100 * percent;
     }
 }
